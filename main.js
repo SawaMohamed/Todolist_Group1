95a411264e8b05c0784e44bdda6ca5d57356b4d0
Bashar Alwarad <b@gmail.com>
2022-06-13 17:55:44 +0200

    adding main layout

---
+++ main.js
@@ -0,0 +1,6 @@
+const layout = document.querySelector('.layout')
+const login = document.querySelector('#login')
+
+login.addEventListener('click',function () {
+  layout.classList.add('hidden')
+})
