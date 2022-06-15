29c1a63050af4ad80c82f19fbf1d931a1dfe847a
Bashar Alwarad <b@gmail.com>
2022-06-15 07:48:28 +0200

    not important

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const layout = document.querySelector('.layout')
 const login = document.querySelector('#login')
+const allDivs=document.querySelectorAll('div')
+console.log(allDivs);
 
-login.addEventListener('click',function () {
+// login.addEventListener('click',function () {
+//   layout.classList.add('hidden')
+// })
+
+function hideLayout() {
   layout.classList.add('hidden')
-})
+}
