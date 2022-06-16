af5a5b199e86637dbb45258d2680077fd828611b
Irina <104638154+IrinaDG@users.noreply.github.com>
2022-06-16 14:27:43 +0200

    Bug-welcome-page

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const layout = document.querySelector('.login-menu')
-const login = document.querySelector('#login')
+const login = document.querySelector('#verification')
 const allDivs=document.querySelectorAll('div')
 
-login.addEventListener('click',function () {
+login.addEventListener('click',function (e) {
+  e.preventDefault();
   layout.classList.add('hidden')
 })
 
+//submit button idea//
+
+let btn = document.querySelector('#interactive');
+
+btn.addEventListener('click', active);
+
+function active() {
+  btn.classList.toggle("is_active");
+}
